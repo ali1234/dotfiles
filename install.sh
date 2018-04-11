5cee121d560b879e6213562344761f200d5c1f7e
Alistair Buxton <a.j.buxton@gmail.com>
2018-04-11 01:53:34 +0100

    Fix theme properties.

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.local/share/themes/
 cp -r Orion ~/.local/share/themes/
 
 xfconf-query -c xsettings -p /Gtk/FontName -s "DejaVu Sans 10"
-xfconf-query -c xsettings -p /Gtk/ThemeName -s "Adwaita"
-xfconf-query -c xsettings -p /Gtk/IconThemeName -s "ubuntu-mono-light"
+xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "ubuntu-mono-light"
 xfconf-query -c xfwm4 -p /general/theme -s "Orion"
 xfconf-query -c xfwm4 -p /general/title_font -s "DejaVu Sans 10"
 xfconf-query -c thunar-volman -p /automount-drives/enabled -s false
