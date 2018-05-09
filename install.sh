f2afe4bd2d9a73f2e204efd793e6eda8180be4eb
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-10 00:15:58 +0100

    Also set font and theme in dconf.
    See https://bugs.launchpad.net/ubuntu/+source/terminator/+bug/1769774

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt purge catfish orage sgt-launcher sgt-puzzles gnome-software thunderbird
-sudo apt install mpv terminator synaptic
+sudo apt install mpv terminator synaptic dconf-cli
 
 # set terminator as default terminal
 cp helpers.rc ~/.config/xfce4/
@@ -27,14 +27,27 @@ mkdir -p ~/.local/share/icons/
 cp -rd ubuntu-mono-light ~/.local/share/icons/
 
 # set misc appearence settings
-xfconf-query -c xsettings -p /Gtk/FontName -s "DejaVu Sans 10"
+xfconf-query -c xsettings -p /Gtk/FontName -s "DejaVu Sans 9.5"
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "ubuntu-mono-light"
 xfconf-query -c xfwm4 -p /general/theme -s "Orion"
-xfconf-query -c xfwm4 -p /general/title_font -s "DejaVu Sans Bold 10"
+xfconf-query -c xfwm4 -p /general/title_font -s "DejaVu Sans Bold 9.5"
 xfconf-query -c thunar-volman -p /automount-drives/enabled -s false
 xfconf-query -c thunar-volman -p /automount-media/enabled -s false
 
-xfpanel-switch load panels/18.04-1-monitor.tar.bz2
+# set same, for gnome
+dconf write /org/gnome/desktop/interface/font-name "'DejaVu Sans 9.5'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Monospace 9.5'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita'"
+dconf write /org/gnome/desktop/interface/icon-theme "'ubuntu-mono-light'"
 
-echo "Don't forget to install userChrome.css"
+
+NMONITORS=$(xrandr --listactivemonitors | head -n 1 | cut -d\  -f 2)
+echo $NMONITORS monitors found.
+
+if [ "$NMONITORS" == "3" ]
+then xfpanel-switch load panels/18.04-3-monitor.tar.bz2
+else xfpanel-switch load panels/18.04-1-monitor.tar.bz2
+fi
+
+echo "Don't forget to install userChrome.css and panel switch"
